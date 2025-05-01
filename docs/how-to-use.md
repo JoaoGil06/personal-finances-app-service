d87337e16e7ceff9ee9719979116544789372800
JoaoGil06 <joaogmonteiro06@gmail.com>
2025-05-01 17:44:50 +0100

    Add docs

---
+++ docs/how-to-use.md
@@ -0,0 +1,4 @@
+This yaml files are not supported by ts compiles, so we need to convert them to JSON.
+In order to do that, we have a command in package json called `compile-api-docs` to compile our yaml to JSON
+
+We also have a command `test-api-docs` to test our docs
--- src/infrastructure/api/config/express.ts
+++ src/infrastructure/api/config/express.ts
@@ -1,12 +1,19 @@
 import express, { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../docs/swagger.json";
 import { userRoutes } from "../../routes/user.route";
 import { authRoutes } from "../../routes/auth.route";
 import { accountRoutes } from "../../routes/account.route";
+import { transactionRoutes } from "../../routes/transaction.route";
 
 export const app: Express = express();
 app.use(express.json());
+
+// Middleware to use swagger to docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
 userRoutes(app);
 authRoutes(app);
 accountRoutes(app);
+transactionRoutes(app);
